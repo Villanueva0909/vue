cdb6e8f2ff8d4795b5fbc244e54795bb30f7ea4f
Raul Villanueva III <106396891+Villanueva0909@users.noreply.github.com>
2026-05-08 16:11:26 +0800

    Adding css

--- tasklist/src/App.vue
+++ tasklist/src/App.vue
@@ -3,8 +3,8 @@
     <h1>Task manager</h1>
 
     <div class="input-row">
-      <input type="text" placeholder="Add task here" v-model="newTask"/>
-      <button>Add task</button>
+      <input type="text" placeholder="Add task here" v-model="newTask" />
+      <button @click="addTask">Add task</button>
     </div>
   </div>
 </template>
@@ -17,11 +17,20 @@ const tasks = ref([''])
 
 function addTask() {
   const text = newTask.value.trim()
-  if(!text){
+  if (!text) {
     return
   }
-  task.value.push()
-} 
+  tasks.value.push({
+    id: Date.now(),
+    text: 'text',
+    completed: false,
+    favorite: false,
+  })
+
+  newTask.value = '';
+
+  console.log(tasks)
+}
 
 </script>
 
@@ -31,6 +40,20 @@ function addTask() {
   margin: 2rem auto;
   font-family: sans-serif;
   text-align: center;
+
+  .input-row {
+    display: flex;
+    gap: 0.5rem;
+    margin-bottom: 1rem;
+  }
+
+  .input {
+    flex-grow: 1;
+    padding: 0.5rem;
+    border-radius: 6px;
+    border: 1px solid;
+  }
+
 
 }
 </style>
