8c76e2ca59529192d955183503504ec87f056a01
Raul Villanueva III <106396891+Villanueva0909@users.noreply.github.com>
2026-05-08 11:59:01 +0800

    first commit

---
+++ tasklist/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrapper">
+    <h1>Task manager</h1>
+
+    <div class="input-row">
+      <input type="text" placeholder="Add task here" v-model="newTask"/>
+      <button>Add task</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const newTask = ref("")
+const tasks = ref([''])
+
+function addTask() {
+  const text = newTask.value.trim()
+  if(!text){
+    return
+  }
+  task.value.push()
+} 
+
+</script>
+
+<style scoped>
+.app {
+  max-width: 500px;
+  margin: 2rem auto;
+  font-family: sans-serif;
+  text-align: center;
+
+}
+</style>
---
+++ vue-count/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Counter is: {{ counter }}</h1>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+</template>
+
+<style scoped></style>
+
+<script setup>
+import { ref } from "vue"
+
+const counter = ref(0)
+
+function decrement() {
+  counter.value--
+}
+
+function increment() {
+  counter.value++
+}
+</script>
